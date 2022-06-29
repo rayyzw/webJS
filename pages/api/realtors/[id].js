2f6d62385266049b8b90dd4361d2431e13141a72
Zewei Yang <rayyzw@gmail.com>
2022-06-28 17:48:04 -0700

    Update realtors API

--- pages/api/realtors/[id].js
+++ pages/api/realtors/[id].js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     if(id){
       try{
         const client = createClient({
-            url: 'redis://default:Nu5tream@127.0.0.1:6379'
+            url: 'redis://default:X1nY@xiny.ca:6379'
         });
         client.on('error', (err) => console.log('Redis Client Error', err));
         await client.connect();
@@ -53,11 +53,13 @@ const getRealtors = async (id) => {
   let pageNumber = 0;  
   while(hasMore){
     pageNumber++;
+    console.log(`page ${pageNumber}`); 
     const url = `https://online.bcfsa.ca/search-results?WCE=C=20|P=up_dirMyEmployees_brn|K=Delears~${id}&page=${pageNumber}`;
-    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
+    const browser = await puppeteer.launch(process.platform==='linux'?{executablePath: '/usr/bin/chromium-browser'}:undefined);
     const page = await browser.newPage();
     await page.goto(url, {
       waitUntil: 'networkidle2',
+      timeout: 0
     });
     
     await page.waitForSelector('table[class="dataTable"]');  
@@ -67,8 +69,7 @@ const getRealtors = async (id) => {
       return document.querySelector(
         'tr[class="webgrid-footer"]'
       )?.innerHTML;
-    }); 
-    console.log(footer);
+    });
 
     if(!footer || String(footer).includes(`</a> ${pageNumber} </td>`))
     {
@@ -80,84 +81,54 @@ const getRealtors = async (id) => {
       hasMore = true;
     } 
 
-    const tableContent = await page.evaluate(() => {
-      debugger;
-      let table = document.querySelector(
-        'table[class="dataTable"]'
-      ).innerHTML;
-      
-      return table;
-    });
-    
-    let hrefs = String(tableContent).split('<td class="Priority-1"><a href="').splice(1).map((href)=>{
-      let data = {};
-      data.name = href.split('">')[1].split('</a>')[0];
-      data.a = `a[href="${href.split('">')[0]}"]`;
-      return data;
+
+    let agentsFullName = await page.evaluate(() => {
+      let res = [];
+      let elements = document.getElementsByClassName("Priority-1");
+      // console.log(elements.length);
+      for (let i = 0; i < elements.length; i++) {
+        if (i == 0) {
+          continue;
+        } else {
+          //each agent
+          elements[i].firstElementChild.click();
+          let name = elements[i].firstElementChild.innerHTML;
+          res.push(name);
+        }
+      }
+      return res;
     });
 
-    for(let i=0;i<hrefs.length;i++){
-      const browser1 = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
-      const page1 = await browser1.newPage();
-      await page1.goto(url, {
-        waitUntil: 'networkidle2',
-      });
-      
-      await page1.waitForSelector('table[class="dataTable"]');      
-      await page1.click(hrefs[i].a);
-      await page1.waitForSelector('tr[class="profilerow"]');
-      const realtor = await page1.evaluate(() => {
-        debugger;
-        let td = document.querySelector(
-          'td[class="directoryprofile"]'
-        ).innerHTML;
+    await page.waitForTimeout(2 * 1000);
 
-        let data = {};
-        if(String(td).includes('<div class="profilelabel">Also Known As:</div><div class="profilevalue">')){
-          data.alsoKnowAs = String(td).split('<div class="profilelabel">Also Known As:</div><div class="profilevalue">')[1].split('</div>')[0];
+    let agentsDetailInfo = await page.evaluate(() => {
+      //read all data from profilewrapper
+      let agentsInfoRes = [];
+      let tables = document.getElementsByClassName("profilerow");
+      for (let index = 0; index < tables.length; index++) {
+        const table = tables[index];
+        let tableInfos = table.querySelectorAll(".profilevalue");
+        let tableLables = table.querySelectorAll(".profilelabel");
+        let agentInfo = {};
+        for (let c = 0; c < tableLables.length; c++) {
+          let labelName = tableLables[c].innerHTML.replace(/[^a-zA-Z]+/g, "");
+          let infoValue = tableInfos[c].innerHTML;
+          agentInfo[`${labelName}`] = infoValue;
         }
-        if(String(td).includes('<div class="profilelabel">Team Name:</div><div class="profilevalue">')){
-          data.teamName = String(td).split('<div class="profilelabel">Team Name:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Licensed As:</div><div class="profilevalue">')){
-          data.licenseAs = String(td).split('<div class="profilelabel">Licensed As:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Licensed For:</div><div class="profilevalue">')){
-          data.licenseFor = String(td).split('<div class="profilelabel">Licensed For:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Licence #:</div><div class="profilevalue">')){
-          data.licenseNumber = String(td).split('<div class="profilelabel">Licence #:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Brokerage:</div><div class="profilevalue">')){
-          data.brokerage = String(td).split('<div class="profilelabel">Brokerage:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Business Address:</div><div class="profilevalue">')){
-          data.businessAddress = String(td).split('<div class="profilelabel">Business Address:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Business Address:</div><div class="profilevalue">')){
-          data.businessAddress = String(td).split('<div class="profilelabel">Business Address:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Business Phone:</div><div class="profilevalue">')){
-          data.businessPhone = String(td).split('<div class="profilelabel">Business Phone:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Business Fax:</div><div class="profilevalue">')){
-          data.businessFax = String(td).split('<div class="profilelabel">Business Fax:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Licence Effective:</div><div class="profilevalue">')){
-          data.licenceEffective = String(td).split('<div class="profilelabel">Licence Effective:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        if(String(td).includes('<div class="profilelabel">Licence Expiry:</div><div class="profilevalue">')){
-          data.licenceExpiry = String(td).split('<div class="profilelabel">Licence Expiry:</div><div class="profilevalue">')[1].split('</div>')[0];
-        }
-        return data;
-      });
-      realtor.name = hrefs[i].name;
-      console.log(realtor);
-      realtors.push(realtor);
-      await browser1.close();
-    }      
+        agentsInfoRes.push(agentInfo);
+      }
+      console.log(agentsInfoRes);
+      return agentsInfoRes;
+    });
+
+    for (let index = 0; index < agentsDetailInfo.length; index++) {
+      agentsDetailInfo[index]['Name'] = agentsFullName[index];
+      realtors.push(agentsDetailInfo[index]);
+    }  
+     
     await browser.close();
   }
 
   return realtors;
 };
+
