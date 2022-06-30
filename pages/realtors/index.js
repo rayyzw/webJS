fc8c8067a102866572a7f7d57a0d92df15836f71
Zewei Yang <rayyzw@gmail.com>
2022-06-30 15:20:39 -0700

    Update commission calculation

--- pages/realtors/index.js
+++ pages/realtors/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useAppContext } from '/components/Context';
+import Modal from '/components/Modal';
+
 export default function Realtors(props) {
   const appContext = useAppContext();
 
@@ -94,73 +96,28 @@ export default function Realtors(props) {
 
   const [realtors, setRealtors] = React.useState(data);
   const [loading, setLoading] = React.useState(true);
-  const [commission, setCommission] = React.useState(0);
   const [id, setId] = React.useState();
+  const [selected, setSelected] = React.useState();
+  const [selectedContent, setSelectedContent] = React.useState();
+
+  const [tiers, setTiers] = React.useState({
+    1: {fixed:0, bonus:0.035},
+    2: {fixed:0.002, bonus:0.038},
+    3: {fixed:0.001, bonus:0.024},
+    4: {fixed:0.001, bonus:0.014},
+    5: {fixed:0.001, bonus:0.009},
+    6: {fixed:0.005, bonus:0.02},
+    7: {fixed:0.005, bonus:0.045}
+  });
+  
 
   React.useEffect(() => {
     setLoading(false);
-    if(loading){      
-      const tiers = {
-        1: {fixed:0, bonus:0.035},
-        2: {fixed:0.002, bonus:0.038},
-        3: {fixed:0.001, bonus:0.024},
-        4: {fixed:0.001, bonus:0.014},
-        5: {fixed:0.001, bonus:0.009},
-        6: {fixed:0.005, bonus:0.02},
-        7: {fixed:0.005, bonus:0.045}
-      }
+    if(loading){
       
-      const getTier = (data) => {
-        if(data && data.children){
-          if(data.children.length<5)
-          {
-            data.tier = 1;
-            data.id = Math.random();
-          }
-          else if(data.children.length<10)
-          {
-            data.tier = 2;
-            data.id = Math.random();
-          }
-          else if(data.children.length<15)
-          {
-            data.tier = 3;
-            data.id = Math.random();
-          }
-          else if(data.children.length<20)
-          {
-            data.tier = 4;
-            data.id = Math.random();
-          }
-          else if(data.children.length<25)
-          {
-            data.tier = 5;
-            data.id = Math.random();
-          }
-          else if(data.children.length<40)
-          {
-            data.tier = 6;
-            data.id = Math.random();
-          }
-          else
-          {
-            data.tier = 7;
-            data.id = Math.random();
-          }
-      
-          for(let i=0;i<data.children.length;i++){
-            getTier(data.children[i]);
-          }
-        }
-        else if(data){
-          data.tier = 1;
-          data.id = Math.random();
-        }
-      }
-
       const getCommission = (data, index, tier) => {
         let commission = 0;
-        if(data && data.children){
+        if(data && data.children && index<7){
           for(let i=0;i<data.children.length;i++){
             let child = data.children[i];
             if(child && child.value>0){
@@ -177,15 +134,68 @@ export default function Realtors(props) {
           }
         }
         return commission;
-      }    
+      };
 
+      const getTier = (data) => {
+        if(data && data.children){
+          if(data.children.length<5)
+          {
+            data.tier = 1;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else if(data.children.length<10)
+          {
+            data.tier = 2;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else if(data.children.length<15)
+          {
+            data.tier = 3;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else if(data.children.length<20)
+          {
+            data.tier = 4;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else if(data.children.length<25)
+          {
+            data.tier = 5;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else if(data.children.length<40)
+          {
+            data.tier = 6;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+          else
+          {
+            data.tier = 7;
+            data.id = Math.random();
+            data.commission = getCommission(data,0,data.tier);
+          }
+      
+          for(let i=0;i<data.children.length;i++){
+            getTier(data.children[i]);
+            data.commission = getCommission(data,0,data.tier);
+          }
+        }
+        else if(data){
+          data.tier = 1;
+          data.id = Math.random();
+          data.commission = getCommission(data,0,data.tier);
+        }
+      }
       getTier(realtors);
       setRealtors({...realtors});
-      console.log("......Calculating Commission......");
-      let c = getCommission(realtors,0,realtors.tier);
-      setCommission(c);
     }
-  }, [loading, realtors]);
+  }, [loading, realtors, tiers]);
 
   
   const createTree = (treeData)=>{
@@ -193,7 +203,14 @@ export default function Realtors(props) {
       return(
         <ul>
           <li style={{display:"flex"}}>
-            <div style={{width:80}}>{treeData.name}</div> 
+            <div style={{width:80}}>
+              {treeData.name}
+            </div> 
+            <div style={{width:80}}>
+              <button onClick={()=>{
+                setSelected(treeData);
+              }}>{parseInt(treeData.commission)}</button>
+            </div> 
             <div style={{width:80, color:colors[treeData.tier]}}>(Tier {treeData.tier})</div>
             <input defaultValue={treeData.value} style={{width:100}} onBlur={(e)=>{
               let realtors1 = {...realtors};
@@ -277,9 +294,41 @@ export default function Realtors(props) {
     return data;
   }
 
+  React.useEffect(() => {
+    if(selected){      
+      const printCommission = (data, index, tier) => {
+        let commission = 0;
+        let printContent = "";
+        if(data && data.children && index<7){
+          for(let i=0;i<data.children.length;i++){
+            let child = data.children[i];
+            if(child && child.value>0){
+              commission = commission + child.value * tiers[(index+1)].fixed;
+              printContent = printContent + child.name;
+              printContent = printContent + "\n";
+              printContent = printContent + "Fixed: " + child.value + " * " + tiers[(index+1)].fixed;
+              printContent = printContent + "\n";
+              if(index<tier){
+                commission = commission + child.value * tiers[(index+1)].bonus;
+                printContent = printContent + "Tier: " + child.value + " * " + tiers[(index+1)].bonus;
+                printContent = printContent + "\n";
+              }
+              printContent = printContent + "---------------";
+              printContent = printContent + "\n";
+            }
+            printContent = printContent + printCommission(child,index+1,tier);
+          }
+        }
+        return printContent;
+      };
+      let content = printCommission(selected,0,selected.tier);
+      setSelectedContent(content);
+    }
+  }, [selected, tiers]);
+
   return (
     <div {...props}>
-      <div className={appContext.styles.titleContainer}>Commission of teamleader: {commission}</div>
+      <div className={appContext.styles.titleContainer}>Commission</div>
       <div className={appContext.styles.tableContainer}>
         {id &&
           <input placeholder='Name' onBlur={(e)=>{
@@ -300,6 +349,14 @@ export default function Realtors(props) {
         }
         {!loading &&
           <>{createTree(realtors)}</>        
+        }
+        {selected &&
+          <Modal onClick={()=>{setSelected(null);}}>
+            <div>
+              {selected.name} = ${selected.commission}<br/> 
+              <textarea value={selectedContent} style={{width:500,height:500}}/>
+            </div>
+          </Modal>
         }
       </div>
     </div>
